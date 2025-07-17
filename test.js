baf22c921e6272dad7637b1df0fa612bb8d837e8
monserratrebolledo <94879940+monserratrebolledo@users.noreply.github.com>
2025-07-17 00:50:49 +0000

    agrega test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {sum} = require('./app.js');
+const {sum, fromEuroToDollar,fromDollarToYen,fromYenToPound} = require('./app.js');
 
 test('adds 14+9 to equal 23', () => {
     let total = sum(14,9);
@@ -20,16 +20,16 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
-test("one dollar be 156.5 yen" , function(){
-    const yen = fromDolarToYen(3.5);
-    const expected = 3.5 * 156.5;
+test("one dollar be approximately 146.26 yenes" , () =>{
+    const yen = fromDollarToYen(1);
+    const expected = (1/1.07) * 156.5;
 
-    expect(fromDollarToYen(3.5)).ToBeCloseTo(547.75,2);
+    expect(yen).toBeCloseTo(expected,2);
 })
 
-test("one JPY be 0.87 GBP", function(){
-    const pound = fromYenToPound(3.5)
-    const expected = 3.5 * 0.87
+test("one yen be approximately 0.00556 pounds", () =>{
+    const pound = fromYenToPound(1)
+    const expected = (1/156.5) * 0.87;
 
-    expected(fromYenToPound(3.5)).ToBeCloseTo(3.845,2)
-})
+    expect(pound).toBeCloseTo(expected, 5);
+});
